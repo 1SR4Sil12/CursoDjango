3d96c6887a3ddcc43ef873934dfcb293c278bbe3
Israel Silva <escorpion45678@gmail.com>
2019-05-18 15:56:20 +0200

    commit 34 FINAL
    Trabajo realizado en el vídeo 39. FINAL DEL PROYECTO

--- pd110/src/boletin/views.py
+++ pd110/src/boletin/views.py
@@ -54,8 +54,9 @@ def inicio(request):
 #		obj.save()
 
 	if request.user.is_authenticated() and request.user.is_staff:
+		queryset = Registrado.objects.all().order_by("-timestamp") #.filter(nombre__iexact="israelsibr")
 		context = {
-			"queryset": ['abc', '123'],
+			"queryset": queryset,
 		}
 	return render(request, "inicio.html", context)
 
